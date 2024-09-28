ccf493cf41f0241b5b0d6bdaed37db8749e0b265
Christopher Shepherd <moi@cshepherd.fr>
2024-09-28 11:32:17 -0400

    fix: some IAC fixes

--- hdtelnet.s
+++ hdtelnet.s
@@ -126,6 +126,7 @@ mainloop    jsr   netin     ; Get a byte frm network
             cmp   #$FF      ; telnet protocol IAC
             bne   maindisp2
             jsr   rcvd_iac
+            bra   localin   ; don't print IAC stuff
 maindisp2   cmp   #$1B      ; is esc?
             bne   maindisp
             jsr   rcvd_esc
@@ -243,17 +244,19 @@ clears      jsr   $FC58
 ; parse the rest
 rcvd_iac    jsr   netin       ; get next byte
             bcc   do_abort    ; no byte? just bail
-            cmp   #$FD        ; DO
+            cmp   #253        ; DO
             beq   iac_do
-            cmp   #$FB        ; WILL
+            cmp   #251        ; WILL
             beq   iac_will
-            cmp   #$FE        ; DON'T
+            cmp   #254        ; DON'T
             beq   iac_dont
-            cmp   #$FC        ; WON'T
+            cmp   #252        ; WON'T
             beq   iac_wont
 do_abort    rts               ; bail (shouldn't happen)
 iac_do      jsr   netin
             bcc   do_abort
+;            cmp   #24         ; terminal ID
+;            beq   send_term   ; send ansi terminal ID
             jmp   send_wont   ; send IAC WON'T for this DO
 iac_will    jsr   netin
             bcc   do_abort
@@ -265,7 +268,7 @@ iac_wont    jsr   netin       ; get WON'T and throw it away
 send_wont   pha
             lda   #$FF        ; IAC
             jsr   out
-            lda   #$FC        ; WON'T
+            lda   #252        ; WON'T
             jsr   out
             pla               ; saved option number
             jsr   out
@@ -273,9 +276,32 @@ send_wont   pha
 send_dont   pha
             lda   #$FF        ; IAC
             jsr   out
-            lda   #$FE        ; DON'T
+            lda   #254        ; DON'T
             jsr   out
             pla               ; saved option number
+            jsr   out
+            rts
+send_term   lda   #$FF        ; IAC
+            jsr   out
+            lda   #250        ; SB
+            jsr   out
+            lda   #24         ; termtype
+            jsr   out
+            lda   #00         ; IS
+            jsr   out
+            lda   #'v'
+            jsr   out
+            lda   #'t'
+            jsr   out
+            lda   #'1'
+            jsr   out
+            lda   #'0'
+            jsr   out
+            lda   #'0'
+            jsr   out
+            lda   #$FF        ; IAC
+            jsr   out
+            lda   #240        ; SE
             jsr   out
             rts
 
